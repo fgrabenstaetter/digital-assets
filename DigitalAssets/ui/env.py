dac7607ba7ad4f2c808eea2657f3fb677eac4b63
François Grabenstaetter <francoisgrabenstaetter@gmail.com>
2019-04-03 10:36:25 +0200

    Change version number

--- DigitalAssets/ui/env.py
+++ DigitalAssets/ui/env.py
@@ -1,6 +1,6 @@
 import pathlib, os
 
 PROGRAM_NAME = 'Digital Assets'
-VERSION = '3.4'
+VERSION = '3.5'
 SHARE_PATH = '/usr/share/digital-assets/'
 CONFIG_PATH = str(pathlib.Path.home()) + '/.config/digital-assets/'
